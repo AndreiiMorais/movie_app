69ff5ea23db25f3944204aeed4fe6359297fb232
Andrei Silveira Morais <andrei.morais@outlook.com>
2022-01-10 12:52:38 -0300

    updates

--- movieapp/lib/pages/homepage.dart
+++ movieapp/lib/pages/homepage.dart
@@ -1,9 +1,12 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:movieapp/controllers/movie_controller.dart';
 import 'package:movieapp/models/movies_model.dart';
 import 'package:movieapp/repositories/movies_repository.dart';
 import 'package:movieapp/repositories/movies_repository_imp.dart';
 import 'package:movieapp/services/dio_service_imp.dart';
+import 'package:movieapp/utils/apis_utils.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -21,33 +24,33 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: EdgeInsets.all(28),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 30),
-            Text(
-              'Movies',
-              style: Theme.of(context).textTheme.headline3,
-            ),
-            Container(
-              height: 544,
-              child: ValueListenableBuilder<Movies?>(
+        padding: const EdgeInsets.all(28),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 30),
+              Text(
+                'Movies',
+                style: Theme.of(context).textTheme.headline3,
+              ),
+              ValueListenableBuilder<Movies?>(
                 valueListenable: _controller.movies,
                 builder: (_, movies, __) {
                   return movies != null
                       ? ListView.separated(
-                          separatorBuilder: (_, __) => Divider(),
+                          physics: NeverScrollableScrollPhysics(),
+                          separatorBuilder: (_, __) => const Divider(),
                           shrinkWrap: true,
                           itemCount: movies.listMovie.length,
-                          itemBuilder: (_, idx) =>
-                              Text(movies.listMovie[idx].title),
+                          itemBuilder: (_, idx) => Image.network(API
+                              .REQUEST_IMG(movies.listMovie[idx].posterPath)),
                         )
                       : Container();
                 },
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
